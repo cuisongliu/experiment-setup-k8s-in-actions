1364a2456cd306cfb6714fedff5b4ccc54cd0de5
cuisongliu <cuisongliu@qq.com>
2022-10-23 22:38:20 +0800

    delete docker remove

--- print_pods.sh
+++ print_pods.sh
@@ -4,7 +4,7 @@ NUM=0
 until [[ $NUM -ne 0 ]]
 do
   sleep 3
-  kubectl get pod --field-selector=status.phase!=Running,status.phase!=Completed
+  kubectl get pod --field-selector=status.phase!=Running,status.phase!=Completed -A
   NUM=$(kubectl get pod --field-selector=status.phase!=Running,status.phase!=Completed -A | grep -v RESTARTS | wc -l)
 done
 kubectl get pod -A
