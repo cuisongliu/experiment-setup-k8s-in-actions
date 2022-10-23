6b244061910bad349aa6afcb9f102b76e948e685
cuisongliu <cuisongliu@qq.com>
2022-10-24 00:15:53 +0800

    add actions for sealos

--- print_pods.sh
+++ print_pods.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -x
 NUM=-999
-maxCount=10
-until [[ $NUM -eq 0 && $maxCount -eq 0 ]]
+until [[ $NUM -eq 0 ]]
 do
   sleep 10
-  maxCount--
   kubectl get pod --field-selector=status.phase!=Running,status.phase!=Completed -A
-  NUM=$(kubectl get pod --field-selector=status.phase!=Running,status.phase!=Completed -A | grep -v RESTARTS | wc -l)
+  NUM=$(kubectl get pod --field-selector=status.phase!=Running,status.phase!=Completed -A | grep -v RESTARTS | grep -v coredns | wc -l)
 done
 kubectl get pod -A
 
