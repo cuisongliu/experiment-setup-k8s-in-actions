ef2bca65b9e8314161a2b7e7e1f12ca42169a878
cuisongliu <cuisongliu@qq.com>
2022-10-23 22:35:33 +0800

    delete docker remove

--- print_pods.sh
+++ print_pods.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+NUM=0
+until [[ $NUM -ne 0 ]]
+do
+  kubectl get pod --field-selector=status.phase!=Running,status.phase!=Completed
+  NUM=$(kubectl get pod --field-selector=status.phase!=Running,status.phase!=Completed -A | grep -v RESTARTS | wc -l)
+  sleep 3
+done
+
 
--- tainit_node.sh
+++ tainit_node.sh
@@ -4,4 +4,3 @@ NODENAME=$(sudo kubectl get nodes -ojsonpath='{.items[0].metadata.name}')
 echo "NodeName=$NODENAME"
 sudo -u root kubectl taint node $NODENAME node-role.kubernetes.io/master-
 sudo -u root kubectl taint node $NODENAME node-role.kubernetes.io/control-plane-
-sudo kubectl get pods -A
