79ec33baee5f62c6faea2e4835b7a17324bc14f1
cuisongliu <cuisongliu@qq.com>
2023-02-10 00:10:44 +0800

    fix actions

--- action.sh
+++ action.sh
@@ -32,6 +32,7 @@ readonly INSTALL_SEALOS_GIT=${sealosGit:-https://github.com/labring/sealos.git}
       git clone $INSTALL_SEALOS_GIT
       sudo apt update && sudo apt install -y libgpgme-dev libbtrfs-dev libdevmapper-dev
       cd sealos
+      go install golang.org/dl/go1.20@latest
       BINS=sealos make build
       BINS=sealctl make build
       sudo chmod a+x bin/linux_amd64/* && sudo mv bin/linux_amd64/* /usr/bin
